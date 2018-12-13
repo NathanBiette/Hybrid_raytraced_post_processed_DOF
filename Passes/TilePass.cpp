65c3d5aafea162bebac9145766c982ac97d7786f
NathanBiette <nathan1.biette@free.fr>
2018-12-13 15:57:28 +0800

    Started to code basic full screen tiling pass inspired by accumulation pass and using FullScreenLaunch wrapper

---
+++ Passes/TilePass.cpp
@@ -0,0 +1,89 @@
+/**********************************************************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+#  * Redistributions of code must retain the copyright notice, this list of conditions and the following disclaimer.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT
+# SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************************************/
+
+#include "TilePass.h"
+
+namespace {
+	// Where is our shader located?
+	const char *kTilingShader = "Tutorial05\\tiling.ps.hlsl";
+};
+
+// Define our constructor methods
+TilePass::SharedPtr TilePass::create(const std::string &bufferToAccumulate)
+{
+	return SharedPtr(new TilePass(bufferToAccumulate));
+}
+
+TilePass::TilePass(const std::string &bufferToAccumulate)
+	: RenderPass("Tiling Pass", "Tiling Options")
+{
+	mAccumChannel = bufferToAccumulate;
+}
+
+bool TilePass::initialize(RenderContext::SharedPtr pRenderContext, ResourceManager::SharedPtr pResManager)
+{
+	if (!pResManager) return false;
+
+	// Stash our resource manager; ask for the texture the developer asked us to accumulate
+	mpResManager = pResManager;
+
+	/* HERE get the rigth texture setup in FBO */
+	mpResManager->requestTextureResource("Tiles", ResourceFormat::R32Float,(Falcor::Resource::BindFlags)112U,20,20);
+	mpResManager->requestTextureResource("Z-Buffer");
+
+	// Create our graphics state and an tiling shader
+	mpGfxState = GraphicsState::create();
+	mpTilingShader = FullscreenLaunch::create(kTilingShader);
+	return true;
+}
+
+void TilePass::resize(uint32_t width, uint32_t height)
+{
+	// Create / resize a texture to store the previous frame.
+	//    Parameters: width, height, texture format, texture array size, #mip levels, initialization data, how we expect to use it.
+	mpLastFrame = Texture::create2D(width, height, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceManager::kDefaultFlags);
+
+	// We need a framebuffer to attach to our graphics pipe state (when running our full-screen pass).  We can ask our
+	//    resource manager to create one for us, with specified width, height, and format and one color buffer.
+	mpInternalFbo = ResourceManager::createFbo(width, height, ResourceFormat::RGBA32Float);
+	mpGfxState->setFbo(mpInternalFbo);
+
+}
+
+void TilePass::execute(RenderContext::SharedPtr pRenderContext)
+{
+	// Get our output buffer; clear it to black.
+	Texture::SharedPtr outputTexture = mpResManager->getClearedTexture("Tiles", vec4(0.0f, 0.0f, 0.0f, 0.0f));
+	Texture::SharedPtr inputTexture = mpResManager->getTexture("Z-Buffer");
+
+	// If our input texture is invalid, or we've been asked to skip accumulation, do nothing.
+	if (!inputTexture) return;
+
+	// Set shader parameters for our accumulation pass
+	auto shaderVars = mpTilingShader->getVars();
+	
+	/*
+	shaderVars["PerFrameCB"]["gAccumCount"] = mAccumCount++;
+	shaderVars["gLastFrame"] = mpLastFrame;
+	shaderVars["gCurFrame"] = inputTexture;
+	*/
+
+	// Execute the accumulation shader
+	mpTilingShader->execute(pRenderContext, mpGfxState);
+
+}
---
+++ Passes/TilePass.h
@@ -0,0 +1,51 @@
+/**********************************************************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+#  * Redistributions of code must retain the copyright notice, this list of conditions and the following disclaimer.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT
+# SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************************************/
+#pragma once
+#include "../SharedUtils/RenderPass.h"
+#include "../SharedUtils/FullscreenLaunch.h"
+
+class TilePass : public RenderPass, inherit_shared_from_this<RenderPass, TilePass>
+{
+public:
+	using SharedPtr = std::shared_ptr<TilePass>;
+
+	static SharedPtr create(const std::string &bufferToAccumulate);
+	virtual ~TilePass() = default;
+
+protected:
+	TilePass(const std::string &bufferToAccumulate);
+
+	// Implementation of SimpleRenderPass interface
+	bool initialize(RenderContext::SharedPtr pRenderContext, ResourceManager::SharedPtr pResManager) override;
+	void execute(RenderContext::SharedPtr pRenderContext) override;
+	void resize(uint32_t width, uint32_t height) override;
+	void stateRefreshed() override;
+
+	// The RenderPass class defines various methods we can override to specify this pass' properties. 
+	bool appliesPostprocess() override { return true; }
+
+	// Information about the rendering texture we're accumulating into
+	std::string                   mAccumChannel;
+
+	// State for our accumulation shader
+	FullscreenLaunch::SharedPtr   mpTilingShader;
+	GraphicsState::SharedPtr      mpGfxState;
+	Texture::SharedPtr            mpLastFrame;
+	Fbo::SharedPtr                mpInternalFbo;
+
+};
