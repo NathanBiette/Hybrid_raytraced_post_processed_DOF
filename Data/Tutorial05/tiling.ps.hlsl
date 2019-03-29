67de68aa713c80a32d34cc992615266a362cf7cb
NathanBiette <nathan1.biette@free.fr>
2019-03-29 16:09:17 +0800

    tiling now tiles only background

--- Data/Tutorial05/tiling.ps.hlsl
+++ Data/Tutorial05/tiling.ps.hlsl
@@ -14,20 +14,23 @@ float4 main(float2 texC : TEXCOORD, float4 pos : SV_Position) : SV_TARGET0
 	uint2 pixelPos = (uint2)pos.xy;
 
 	float max_coc = 0.0f;
-	float nearest_Z = gZBuffer[uint2(pixelPos.x * 20, pixelPos.y * 20)].r;
+	//float nearest_Z = gZBuffer[uint2(pixelPos.x * 20, pixelPos.y * 20)].r;
+	//float nearestZFar = gZBuffer[uint2(pixelPos.x * 20, pixelPos.y * 20)].r * (gZBuffer[uint2(pixelPos.x * 20, pixelPos.y * 20)].r > gDistanceToFocalPlane);
+	float nearestZFar = 0.0f;
 
 	for (int x = pixelPos.x * 20; x < pixelPos.x * 20 + 20; x++) {
 		for (int y = pixelPos.y * 20; y < pixelPos.y * 20 + 20; y++) {
-			if (gZBuffer[uint2(x,y)].r < nearest_Z) {
-				nearest_Z = gZBuffer[uint2(x, y)].r;
-			}
-			if (COC(gZBuffer[uint2(x, y)].r) > max_coc) {
-				max_coc = COC(gZBuffer[uint2(x, y)].r);
-			}
+			nearestZFar += (gZBuffer[uint2(x, y)].r > gDistanceToFocalPlane) 
+				* ((nearestZFar > 0.0f 
+					&& gZBuffer[uint2(x, y)].r < nearestZFar)
+				* (gZBuffer[uint2(x, y)].r - nearestZFar)
+				+ (nearestZFar == 0.0f)
+				* (gZBuffer[uint2(x, y)].r - nearestZFar));
+			max_coc = max(max_coc, COC(gZBuffer[uint2(x, y)].r) * (gZBuffer[uint2(x, y)].r > gDistanceToFocalPlane));
 		}
 	}
 
-	float4 outColor = float4(max_coc, nearest_Z, 0.0f, 1.0f);
+	float4 outColor = float4(max_coc, nearestZFar, 0.0f, 1.0f);
 
 	return outColor;
 }
