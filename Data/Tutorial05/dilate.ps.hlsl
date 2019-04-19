a63966019a21e7b98be6987279d04799041e4b3b
NathanBiette <nathan1.biette@free.fr>
2019-04-19 16:43:02 +0800

    Cleaned code in dilate pass and added foreground nearest Z in tile and max COC in tile

--- Data/Tutorial05/dilate.ps.hlsl
+++ Data/Tutorial05/dilate.ps.hlsl
@@ -19,11 +19,13 @@ PS_OUTPUT main(float2 texC : TEXCOORD, float4 pos : SV_Position)
 	PS_OUTPUT DilatePassOutput;
 	uint2 pixelPos = (uint2)pos.xy;
 
-	float max_coc = 0.0f;
-	float max_coc_RT = 0.0f;
-	float nearest_Z = 0.0f;
+	float maxBackgroundCOC = 0.0f;
+	float maxForegroundCOC = 0.0f;
+	float nearestBackgroundZ = 0.0f;
+	float nearestForegroundZ = 0.0f;
 	bool raytraceTile = false;
 
+	// Handle image border during sampling
 	int start_x = 0;
 	int start_y = 0;
 	if (pixelPos.x > 0) {
@@ -32,22 +34,31 @@ PS_OUTPUT main(float2 texC : TEXCOORD, float4 pos : SV_Position)
 	if (pixelPos.y > 0) {
 		start_y = pixelPos.y - 1;
 	}
-
 	int stop_x = min(pixelPos.x + 1, width - 1);
 	int stop_y = min(pixelPos.y + 1, height - 1);
 
-	//Iterate through 3x3 neighbourhood and find nearest Z in tile in neighbourhood
-
+	// Iterate through 3x3 neighbourhood and find nearest Z in tile in neighbourhood
 	for (int i = start_x; i < stop_x + 1; i++) {
 		for (int j = start_y; j < stop_y + 1; j++) {
-			max_coc = max(max_coc, gTiles[uint2(i, j)].r);
-			max_coc_RT = max(max_coc_RT, gRaytraceTiles[uint2(i, j)].g);
-			nearest_Z += (gTiles[uint2(i, j)].g > 0.0f) * ((gTiles[uint2(i, j)].g - nearest_Z) * (nearest_Z > 0.0f && gTiles[uint2(i, j)].g < nearest_Z) + gTiles[uint2(i, j)].g * (nearest_Z == 0.0f));
+
+			maxBackgroundCOC = max(maxBackgroundCOC, gTiles[uint2(i, j)].r);
+			maxForegroundCOC = max(maxForegroundCOC, gTiles[uint2(i, j)].b);
+
+			nearestBackgroundZ += (gTiles[uint2(i, j)].g > 0.0f) 
+				* ((gTiles[uint2(i, j)].g - nearestBackgroundZ) 
+				* (nearestBackgroundZ > 0.0f && gTiles[uint2(i, j)].g < nearestBackgroundZ) 
+				+ gTiles[uint2(i, j)].g * (nearestBackgroundZ == 0.0f));
+
+			nearestForegroundZ += (gTiles[uint2(i, j)].a > 0.0f)
+				* ((gTiles[uint2(i, j)].a - nearestForegroundZ)
+				* (nearestForegroundZ > 0.0f && gTiles[uint2(i, j)].a < nearestForegroundZ)
+				+ gTiles[uint2(i, j)].a * (nearestForegroundZ == 0.0f));
+
 			raytraceTile = raytraceTile || gRaytraceTiles[uint2(i, j)].r > 0.0f;
 		}
 	}
-	DilatePassOutput.dilatedTiles = float4(max_coc, nearest_Z, 0.0f, 1.0f);
-	//DilatePassOutput.raytraceMask = gRaytraceTiles[pixelPos].r < distToFocusPlane ? 1.0f : 0.0f;
-	DilatePassOutput.raytraceMask = float4(raytraceTile, max_coc_RT, 0.0f, 1.0f);
+
+	DilatePassOutput.dilatedTiles = float4(maxBackgroundCOC, nearestBackgroundZ, maxForegroundCOC, nearestForegroundZ);
+	DilatePassOutput.raytraceMask = float4(raytraceTile, maxForegroundCOC, 0.0f, 1.0f);
 	return DilatePassOutput;
 }
